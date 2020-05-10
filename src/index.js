65a9d61556318a7ffe6e05f9cc4a54fba44b3237
Amauri Antonio de Oliveira <amauriibate32@hotmail.com>
2020-05-09 23:35:21 -0300

    Inicio do projeto

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const routes = require('./routes');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(morgan('dev'));
+
+app.use(routes);
+
+app.listen(process.env.PORT || 3000);
